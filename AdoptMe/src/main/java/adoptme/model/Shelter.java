260b53ff9d0add8f2664b1da45418408bd793dbf
ebras <ebras@192.168.1.193>
2025-05-04 23:52:19 -0500

    create generic Shelter<T extends Pet> to manage adoptable pets

--- AdoptMe/src/main/java/adoptme/model/Shelter.java
+++ AdoptMe/src/main/java/adoptme/model/Shelter.java
@@ -1,5 +1,41 @@
 package adoptme.model;
 
-public class Shelter {
+import java.util.*;
 
+/**
+ * Generic class to store and manage pets.
+ */
+public class Shelter<T extends Pet> {
+    private List<T> pets = new ArrayList<>();
+
+    public void addPet(T pet) {
+        pets.add(pet);
+    }
+
+    public void removePet(T pet) {
+        pets.remove(pet);
+    }
+
+    public List<T> getAllPets() {
+        return pets;
+    }
+
+    public void sortByName() {
+        Collections.sort(pets);
+    }
+
+    public void sortByAge() {
+        pets.sort(Comparator.comparingInt(Pet::getAge));
+    }
+
+    public void sortBySpecies() {
+        pets.sort(Comparator.comparing(Pet::getSpecies));
+    }
+
+    public T getPetById(int id) {
+        for (T pet : pets) {
+            if (pet.getId() == id) return pet;
+        }
+        return null;
+    }
 }
